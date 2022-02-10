20e52cc4cac7281bc29c419056f57a6665097f3a
Somesh <ogres-pater.0v@icloud.com>
2022-02-10 11:20:22 -0600

    minor update

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,9 @@
-FROM node:alpine
-
-COPY ./ ./
+FROM node:16-alpine as builder
+WORKDIR '/app'
+COPY package.json .
 RUN npm install
-
 COPY . .
+RUN npm run build
 
-EXPOSE 8080
-
-CMD [ "node", "server.js" ]
+FROM nginx
+COPY --from=builder /app/build /usr/share/nginx/html
--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,9 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Bye there!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
